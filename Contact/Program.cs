8694e82f0cee90e5faca912dd0b2bdbe8a666269
Marko <marko_milic@outlook.com>
2017-11-23 19:38:16 +0100

    Add project files.

---
+++ Contact/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace Contact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String email = "";
+            
+            Console.WriteLine("Enter e-mail");
+            email = Console.ReadLine();
+            doTheThing(email);
+
+            Console.ReadKey();
+        }
+        public static async Task doTheThing(String email)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("X-FullContact-APIKey", "256d64494c066cee");
+            String response = await client.GetStringAsync("https://api.fullcontact.com/v2/person.json?email=" + email);
+            String[] data = response.Split(":");
+        
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i].Contains("likelihood"))
+                {
+                    Console.WriteLine("likelihood" + data[i + 1]);
+                }
+            }
+
+            Console.WriteLine(response);
+        }
+    }
+}
+
---
+++ FullContactAPI/IFullContactApi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullContactAPI
+{
+    interface IFullContactApi
+    {
+        Task<FullContactPerson> lookupPersonByEmailAsync(String email);
+    }
+
+    public class FullContactPerson
+    {
+        public string likeIHood = "";
+        public string contactInfo;
+    }
+
+    public static class contactPerson : IFullContactApi
+    {
+        public async Task<FullContactPerson> lookupPersonByEmailAsync(string email)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("X-FullContact-APIKey", "256d64494c066cee");
+            String response = await client.GetStringAsync("https://api.fullcontact.com/v2/person.json?email=" + email);
+            String[] data = response.Split(":");
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i].Contains("likelihood"))
+                {
+                    Console.WriteLine("likelihood" + data[i + 1]);
+                }
+            }
+            throw new NotImplementedException();
+        }
+    }
+
+}
